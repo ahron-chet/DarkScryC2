0c1a9473aa25240f47968efee6d7668e9680b182
Aahron Chetrit <105120555+ahron-chet@users.noreply.github.com>
2025-07-03 20:32:44 +0300

    Add gtest-based logger test

---
+++ CPPclient/tests/test_logger.cpp
@@ -0,0 +1,27 @@
+#include "Logger/Logger.h"
+#include <gtest/gtest.h>
+#include <fstream>
+#include <cstdio>
+
+TEST(LoggerTest, WritesToFile) {
+    const char* logPath = "test.log";
+    std::remove(logPath);
+
+    CppAgent::Logger logger(false, true, logPath);
+    std::string msg = "sample message";
+    logger.log(msg, CppAgent::Logger::Level::Info);
+
+    std::ifstream file(logPath);
+    ASSERT_TRUE(file.is_open());
+
+    std::string line;
+    std::getline(file, line);
+    file.close();
+
+    EXPECT_NE(line.find(msg), std::string::npos);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
