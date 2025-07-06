9b6a70c526f7bb9d912b43bd71a784910d376ce4
Aahron Chetrit <105120555+ahron-chet@users.noreply.github.com>
2025-07-06 17:37:02 +0300

    Update __init__.py

--- Management/app/__init__.py
+++ Management/app/__init__.py
@@ -7,7 +7,7 @@ from .routers import agents, auth, users
 def create_app() -> FastAPI:
     settings = get_settings()
     app = FastAPI(debug=settings.debug)
-    app.include_router(auth.router, description="Agents management")
+    app.include_router(auth.router)
     app.include_router(users.router)
     app.include_router(agents.router)
     return app
