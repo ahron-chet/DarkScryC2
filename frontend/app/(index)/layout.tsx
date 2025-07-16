bbbfd225bd77835dd6689a27370586bbf0c50404
Aahron Chetrit <105120555+ahron-chet@users.noreply.github.com>
2025-07-16 21:19:47 +0300

    Update layout.tsx

--- frontend/app/(index)/layout.tsx
+++ frontend/app/(index)/layout.tsx
@@ -7,6 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "animate.css/animate.css";
 import "./../../style/components.Sidebar.css";
+import Script from 'next/script';
 
 export default function WithSidebarLayout({
   children,
@@ -16,10 +17,10 @@ export default function WithSidebarLayout({
   useRequireAuth();
   return (
     <>
-      {/* <Script
+      <Script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
         strategy="afterInteractive"
-      /> */}
+      />
       <Sidebar />
       <main className="main-content">{children}</main>
     </>
