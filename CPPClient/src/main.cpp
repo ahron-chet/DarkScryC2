9f95dd9f8542fe4e0c76c082f095e1c18ccea797
Aahron Chetrit <105120555+ahron-chet@users.noreply.github.com>
2025-07-20 18:36:30 +0300

    =

--- CPPClient/src/main.cpp
+++ CPPClient/src/main.cpp
@@ -1,16 +1,8 @@
 #define NOMINMAX
 #include "Agent.h"
-#include "win32/include/UserUtils.hpp"
 
 int main() {
-    std::wstring sid = win32::user::get_sid_by_user_name(L"Administrator");
-
-    if (!sid.empty()) {
-        std::wcout << L"Administrator SID: " << sid << std::endl;
-    }
-    else {
-        std::wcout << L"Failed to get SID for Administrator." << std::endl;
-    }
+    
     CppAgent::Agent agent;
     bool ok = agent.run();
     return ok ? 0 : 1;
