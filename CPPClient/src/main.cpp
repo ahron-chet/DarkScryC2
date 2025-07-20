a9a458d7778daf2995a409553c8061390915d782
Aahron Chetrit <105120555+ahron-chet@users.noreply.github.com>
2025-07-20 18:36:06 +0300

    Fix Linux build by conditionally including Windows-specific code

--- CPPClient/src/main.cpp
+++ CPPClient/src/main.cpp
@@ -1,16 +1,21 @@
 #define NOMINMAX
 #include "Agent.h"
-#include "win32/include/UserUtils.hpp"
+
+#ifdef _WIN32
+#  include "win32/include/UserUtils.hpp"
+#endif
 
 int main() {
+#ifdef _WIN32
     std::wstring sid = win32::user::get_sid_by_user_name(L"Administrator");
 
     if (!sid.empty()) {
         std::wcout << L"Administrator SID: " << sid << std::endl;
-    }
-    else {
+    } else {
         std::wcout << L"Failed to get SID for Administrator." << std::endl;
     }
+#endif
+
     CppAgent::Agent agent;
     bool ok = agent.run();
     return ok ? 0 : 1;
