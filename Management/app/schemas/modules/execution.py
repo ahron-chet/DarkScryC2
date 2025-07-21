d218e7adf24fc32e7a03a043cabcbd1d557b5fc6
Aahron Chetrit <105120555+ahron-chet@users.noreply.github.com>
2025-07-21 13:38:57 +0300

    Refine StartShell schema

--- Management/app/schemas/modules/execution.py
+++ Management/app/schemas/modules/execution.py
@@ -1,25 +1,35 @@
-from pydantic import BaseModel, Field
+from enum import Enum
 from typing import Optional
-from darkscryc2server.models.messages import (
-    CommandIdentifiers
-)
+
+from darkscryc2server.models.messages import CommandIdentifiers
+from pydantic import BaseModel, Field
+
+
+class ShellCreationType(str, Enum):
+    current_user = "current_user"
+    impersonate_token_duplicate = "impersonate_token_duplicate"
+    credentials = "credentials"
+
 
 class StartShellCommand(BaseModel):
     file_name: Optional[str] = Field(
         "cmd.exe",
-        title="Session Name",
-        description="Optional name for the shell session."
+        description="Optional name for the shell session.",
     )
     user_name: Optional[str] = Field(
         None,
-        title="Session Name",
-        description="Optional name for the shell session."
+        description="Optional username when using credentials.",
+    )
+    password: Optional[str] = Field(
+        None,
+        description="Optional password used with credentials mode.",
+    )
+
+    creation_type: ShellCreationType = Field(
+        default=ShellCreationType.current_user,
+        description="How to create the shell session",
     )
 
 
 class RunCommand(BaseModel):
-    command: str = Field(
-        ...,
-        title="Command",
-        description="The command to be executed."
-    )
+    command: str = Field(..., description="The command to be executed.")
